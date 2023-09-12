ce2666b39c2dfd16579f0b8180b80432e9b119f9
oleksiiKAV <oleksii.kav@gmail.com>
2023-09-12 12:27:10 +0300

    login_reg images+fonts

--- App.js
+++ App.js
@@ -1,28 +1,14 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { RegistrationScreen } from './Screens/login/RegistrationScreen';
+import { LoginScreen } from './Screens/login/LoginScreen';
+import { StatusBar } from 'expo-status-bar';
 
-export default App = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>React Native</Text>
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: "#eaeaea"
-  },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: "#20232a",
-    borderRadius: 14,
-    backgroundColor: "#61dafb",
-    color: "#20232a",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold"
-  }
-});
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <LoginScreen />
+      {/* <RegistrationScreen /> */}
+      
+    </>
+  );
+}
