473f343067e23d4a618408cdc6dd730ac41b2db7
oleksiiKAV <oleksii.kav@gmail.com>
2023-09-12 14:46:04 +0300

    hw3-4 screens template + navigation

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
 import { RegistrationScreen } from './Screens/login/RegistrationScreen';
 import { LoginScreen } from './Screens/login/LoginScreen';
-import { StatusBar } from 'expo-status-bar';
+import { Home } from './Screens/home/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/ttf/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./assets/ttf/Roboto-Medium.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const MainStack = createStackNavigator();
+
   return (
     <>
       <StatusBar style="auto" />
-      <LoginScreen />
-      {/* <RegistrationScreen /> */}
-      
+      <NavigationContainer>
+        <MainStack.Navigator initialRouteName="LoginScreen">
+          {/* <MainStack.Screen
+            name="RegistrationScreen"
+            component={RegistrationScreen}
+            options={{ headerShown: false }}
+          /> */}
+          {/* <MainStack.Screen
+            name="LoginScreen"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />  */}
+          <MainStack.Screen
+            name="HomeScreen"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+        </MainStack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
